bc2e7ab8380797ab1fe2cadea6de51cd8b35a31a
Dima <krohalevdmitrij934@gmail.com>
2025-11-24 20:24:31 +0500

    исправление ошибок импорта

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/country/CountryCodeBottomSheet.kt
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/country/CountryCodeBottomSheet.kt
@@ -1,24 +1,14 @@
 package ru.yandex.practicum.contacts.presentation.country
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,6 +17,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import ru.yandex.practicum.contacts.R
 import ru.yandex.practicum.contacts.data.models.CountryCode
+import ru.yandex.practicum.contacts.presentation.ui.components.CommonBottomSheet
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,9 +30,7 @@ fun CountryCodeBottomSheet(
         title = stringResource(R.string.filter_by_country_code),
         items = CountryCode.COMMON_CODES,
         selectedItems = selectedCodes,
-        onItemsSelected = { selected ->
-            selected.firstOrNull()?.let { onCodesSelected(it) }
-        },
+        onItemsSelected = onCodesSelected,
         onDismiss = onDismiss
     ){countryCode, isSelected ->
         CountryCodeOption(
